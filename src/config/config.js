a0caff7eb7b3f4927f163f2270b8f3536a48d133
Himanshu <himanshugugria@gmail.com>
2025-07-11 21:51:29 +0530

    setting up database

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+// this is a file to use env variables 
+// a better method to use env variables in production 
+
+const config = {
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+export default config;
